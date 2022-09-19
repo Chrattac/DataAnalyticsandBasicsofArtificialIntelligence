7a135e00dfe6e7cf168c75d958961c4030c5bbc4
Chrattac <juho.jansson@tuni.fi>
2022-09-20 00:47:10 +0300

    Revert "Week 37 done"
    This reverts commit 764e092cbfe04e5698b0dcc773e2353f8ab77cbc.

--- week37/ex2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as mpl
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv('emp-dep.csv')
-
-mpl.bar(df.age_group.unique(), df.age_group.value_counts(), width=2)
-mpl.show()
